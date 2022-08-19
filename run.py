6c559c1de92f3468be850e26078d1c32c283a9d7
Shantanu Deshmukh <shantanudeshmukh2003@gmail.com>
2022-08-19 10:31:11 +0530

    Added basic python script

---
+++ run.py
@@ -0,0 +1,113 @@
+#!/bin/python3
+from os.path import exists, isfile
+import subprocess, argparse, json, os, sys
+
+ps = subprocess.Popen(('lshw', '-C', 'display'), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+output = subprocess.check_output(('grep', 'vendor'), stdin=ps.stdout, encoding="UTF-8")
+ps.wait()
+
+if "nvidia" in output.lower():
+    COMPOSE_FILE = "docker-compose-nvidia.yml"
+else:
+    COMPOSE_FILE = "docker-compose.yml"
+
+def build(info, workspace_path):
+    subprocess.run(f"docker-compose -f {COMPOSE_FILE} build".split())
+    info[os.path.abspath(workspace_path)] = False
+
+def list(info, running):
+    print(f"Currently active workspaces: {len(info)}")
+    for n, w in enumerate(info):
+        if w in running:
+            r = "Running"
+        else:
+            r = "Not running"
+        print(f"{n+1} - {w} - {r}")
+
+def rm(info, workspace):
+    if workspace.isdecimal():
+        workspace = int(workspace)
+        if workspace > len(info):
+            k = info.pop(workspace)
+            print(f"Workspace \"{k}\" removed")
+        else:
+            print(f"Workspace #{workspace} does not exist!", file=sys.stderr)
+            exit(1)
+    else:
+        if workspace in info.keys():
+            info.remove(workspace)
+            print(f"Workspace \"{workspace}\" removed")
+        else:
+            print(f"Workspace \"{workspace}\" does not exist!", file=sys.stderr)
+            exit(1)
+
+if __name__ == "__main__":
+    LOCK_FILE_PATH = "/tmp/ros-docker.lock"
+    INFO_FILE_PATH = os.environ["HOME"] + "/.config/ros-docker/ros-docker.json"
+    RUNNING_FILE_PATH = "/tmp/ros-docker"
+
+    parser = argparse.ArgumentParser(description="Manage docker containers for ROS")
+    subparsers = parser.add_subparsers(dest="subcommand")
+
+    build_parser = subparsers.add_parser("build", help="Build a docker container for a workspace")
+    build_parser.add_argument("path", metavar="PATH", help="Path to the workspace", default='.')
+
+    build_parser = subparsers.add_parser("rm", help="Delete a workspace")
+    build_parser.add_argument("workspace", metavar="WORKSPACE", help="Remove a workspace by path or number", action="store")
+
+    build_parser = subparsers.add_parser("run", help="Run a docker container for a workspace")
+    build_parser.add_argument("workspace", metavar="WORKSPACE", help="Path or number to workspace", default='.')
+
+    build_parser = subparsers.add_parser("list", help="List ros-docker workspaces")
+
+    args = parser.parse_args()
+
+    try:
+        lock_file = open(LOCK_FILE_PATH, 'x')
+    except FileExistsError:
+        print(f"The file {LOCK_FILE_PATH} exists\nThis means that the program may not have successfully exited the previous time it ran")
+        yn = input("Would you like to delete the file and contiinue regardless? [y/N]: ")
+        if yn.lower() != 'y':
+            exit(1)
+        lock_file = open(LOCK_FILE_PATH)
+
+    if not os.path.exists(os.path.dirname(INFO_FILE_PATH)):
+        os.makedirs(os.path.dirname(INFO_FILE_PATH))
+
+    info = []
+    if os.path.exists(INFO_FILE_PATH):
+        info_file = open(INFO_FILE_PATH, 'r')
+        info = json.load(info_file)
+        info_file.close()
+    else:
+        info_file = open(INFO_FILE_PATH, 'x+')
+        json.dump({}, info_file)
+        info_file.close()
+
+    running = []
+    if os.path.exists(RUNNING_FILE_PATH):
+        running_file = open(RUNNING_FILE_PATH, 'r')
+        running = json.load(running_file)
+        running_file.close()
+    else:
+        running_file = open(RUNNING_FILE_PATH, 'x+')
+        json.dump({}, running_file)
+        running_file.close()
+
+    match args.subcommand:
+        case "build":
+            build(info, args.path)
+        case "run":
+            pass
+        case "list":
+            list(info, running)
+        case "rm":
+            rm(info, args.workspace)
+
+    info_file = open(INFO_FILE_PATH, 'w')
+    json.dump(info, info_file)
+    info_file.close()
+
+    lock_file.close()
+    info_file.close()
+    os.remove(LOCK_FILE_PATH)
